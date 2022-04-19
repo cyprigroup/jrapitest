70604106f7ed1b29f8c2ede934e864b1353dfff2
Eric Muehlstein <eric@cyprigroup.com>
2022-04-19 13:44:01 -0500

    adding JR library code

--- jrnotebooks/historical_data.py
+++ jrnotebooks/historical_data.py
@@ -26,7 +26,74 @@ class HDParams:
     def set_start_date(self, datestr):
         self.start_date = datetime.fromisoformat(datestr)
 
+class JRParams:
+    '''
+    Collect feature information
+    '''
+    def __init__(
+        self,
+        dv="ETH-USD",
+        mbs=240,
+        feature_name="default",
+        value="",
+        ipp="last",
+    ):
 
+        self.dv = dv
+        self.mbs = mbs
+        self.feature_name = feature_name
+        self.value = value
+        self.ipp = ipp
+
+#    def set_start_date(self, datestr):
+#        self.start_date = datetime.fromisoformat(datestr)
+
+class JudgeResearch:
+    '''
+    Conduit to JudgeResearch API
+    '''
+    def __init__(self):
+        self.url_root = ""
+        self.api_key = ""
+        
+    def with_api_key(self, key):
+        self.api_key = key 
+        
+    def fmt_time(self, timestamp):
+        ''' format time per API doc '''
+        return datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    def send_feature(self, jrparams, data):
+        '''
+        Send feature data point given a JRParams object and a tuple or list of tuples containing startdate & value
+        '''
+        
+        _url = (
+            self.url_root
+            + "api_key="
+            + self.api_key
+            + "&exchange="
+            + asset.exchange
+            + "&ticker="
+            + asset.ticker
+            + "&startDate="
+            + str(int(asset.start_date.timestamp() * 1000))
+            + "&interval="
+            + asset.interval
+            + "&numPeriods="
+            + str(asset.num_periods)
+        )
+
+        r = requests.get(_url)
+        if r.status_code == 200:
+            _json = r.json()
+            data = _json["data"]
+            return data
+        else:
+            print("Error fetching data")
+            return r.text
+        
+    
 class Coinalytix:
     """
     Conduit to Coinalytix API
