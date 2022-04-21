a7ea761af4f3caa5efcfa0cccec98cffb2817862
Eric Muehlstein <eric@cyprigroup.com>
2022-04-21 09:44:30 -0500

    jr lib updates and sample code with working submission of single feature

--- JudgeResearchNotebooks/judgeresearch.py
+++ JudgeResearchNotebooks/judgeresearch.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
 
+import requests
+
 class JRParams:
     """
     Collect feature information
@@ -32,18 +34,33 @@ class JudgeResearch:
     """
 
     def __init__(self):
-        self.url_root = ""
+        self.url = "https://ct6gu86m3d.execute-api.us-east-2.amazonaws.com/dev"
         self.api_key = ""
 
     def with_api_key(self, key):
         self.api_key = key
+        self.headers = {"x-api-key": key}
 
     def fmt_time(self, timestamp):
         """format time per API doc"""
         return datetime.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    def format_payload(featuredata):
-        pass
+    def format_payload(self, features):
+        """Accepts list of jrparam objects and generates JSON payload"""
+        _data = []
+        for feature in features:
+            _payload = {
+                "attribute": feature.attribute,
+                "DV": feature.dv,
+                "MBS": feature.mbs,
+                "featureName": feature.feature_name,
+                "value": feature.value,
+                "IPP": feature.ipp,
+            }
+            _data.append(_payload)
+        data = {"data": _data}
+        return data
 
-    def submit_feature(payload):
-        pass
+    def submit_feature(self, data):
+        featureupdate = requests.put(self.url, headers=self.headers, json=data)
+        return featureupdate
